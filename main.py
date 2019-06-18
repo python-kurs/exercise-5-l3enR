404d39fba8937edf300257be00677ca6bcfef68c
BenR <44396287+BenR32@users.noreply.github.com>
2019-06-18 11:44:23 +0200

    Ass5

--- main.py
+++ main.py
@@ -12,19 +12,48 @@ output_dir = Path("solution")
 #    and download the 0.25 deg. file for daily mean temperature.
 #    Save the file into the data directory but don't commit it to github!!! [2P]
 
-
 # 2. Read the file using xarray. Get to know your data. What's in the file?
 #    Calculate monthly means for the reference periode 1981-2010 for Europe (Extent: Lon_min:-13, Lon_max: 25, Lat_min: 30, Lat_max: 72). [2P]
 
+data_dir = input_dir / "tg_ens_mean_0.25deg_reg_v19.0e.nc"
+data = xr.open_dataset(data_dir)
+
+ref_dat = data.sel(latitude = slice(30,72), longitude = slice(-13,25), time = slice("1981-01-01","2010-12-31"))
+
+#test_dat = ref_dat.sel(time = "2002-06-01")
+#test_dat["tg"].plot(x = "longitude")
+
+monMean = ref_dat["tg"].groupby("time.month").mean("time")
+
+#test = monMean.sel(month = 1)
+#test.plot(x = "longitude")
 
 # 3. Calculate monthly anomalies from the reference period for the year 2018 (use the same extent as in #2).
 #    Make a quick plot of the anomalies for the region. [2P]
 
+anomaly = data.sel(latitude = slice(30,72), longitude = slice(-13,25), time = "2018")
+anomaly = anomaly["tg"].groupby("time.month").mean("time")
+#anomaly = 2018 - monMean
+anomaly = anomaly - monMean
+anomaly.plot(x = "longitude", col = "month", col_wrap = 3)
 
 # 4. Calculate the mean anomaly for the year 2018 for Europe (over all pixels of the extent from #2) 
 #    Compare this overall mean anomaly to the anomaly of the pixel which contains Marburg. 
 #    Is the anomaly of Marburg lower or higher than the one for Europe? [2P] 
 
+from utils import mean_anomaly
+
+mean_ano_18 = mean_anomaly(x = data, lat_min = 30, lat_max = 72, lon_min = -13, lon_max = 25, year = 2018)
+#mean_ano_18.plot(x = "longitude")
+
+#Marburg = 50.85/8.77
+#umschließendes Pixel: (50.75 - 51 / 8.75 - 9)
+mr_anomaly = mean_anomaly(x = data, lat_min = 50.75, lat_max = 51, lon_min = 8.75, lon_max = 9, year = 2018)
+#mr_anomaly.plot()
 
 # 5. Write the monthly anomalies from task 3 to a netcdf file with name "europe_anom_2018.nc" to the solution directory.
 #    Write the monthly anomalies for Marburg to a csv file with name "marburg_anom_2018.csv" to the solution directory. [2P]
+anomaly.to_netcdf("solution/europe_anom_2018.nc")
+
+mr_anomaly = mr_anomaly.to_dataframe()
+mr_anomaly.to_csv("solution/marburg_anom_2018.csv")
--- utils.py
+++ utils.py
@@ -2,3 +2,56 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def mean_anomaly(x, lat_min, lat_max, lon_min, lon_max, year):
+    """
+    Calculates the mean anomaly from the monthly anomaly values.
+    
+    The single monthly anomalies were calculated and used in a mean function.
+    
+    Parameters
+    ----------
+    x       : xarray Dataset
+              The original dataset
+    lat_min : number
+              The minimum latitude extent 
+    lat_max : number
+              The maximum latitude extent 
+    lon_min : number
+              The minimum longitude extent 
+    lon_max : number
+              The maximum longitude extent 
+    year    : number
+              The requested year        
+            
+    Returns
+    -------
+    xarray Dataset
+    
+    """
+    #create a month vector
+    import numpy as np
+    month = np.arange(1,13)
+    
+    #create 12 arrays for the 12 month and groupby month
+    arrayDict = {}
+    
+    for i in month:
+        if i < 10:
+            temp = x.sel(latitude = slice(lat_min,lat_max), longitude = slice(lon_min, lon_max), time = str(year)+"-0"+str(i))
+            arrayDict["mon"+str(i)] = temp["tg"].groupby("time.month").mean("time")
+        else:
+            temp = x.sel(latitude = slice(lat_min,lat_max), longitude = slice(lon_min, lon_max), time = str(year)+"-"+str(i))
+            arrayDict["mon"+str(i)] = temp["tg"].groupby("time.month").mean("time")
+        
+    #sum up the monthly anomalies
+    for i in month:
+        if i == 1:
+            annualSum = arrayDict["mon"+str(i)]
+        else:
+            annualSum = sum(annualSum, arrayDict["mon"+str(i)]) #use the sum() function because the "+" produce errors 
+    
+    #divide the result by the number of month    
+    anomaly_total = annualSum / 12
+    
+    return anomaly_total
